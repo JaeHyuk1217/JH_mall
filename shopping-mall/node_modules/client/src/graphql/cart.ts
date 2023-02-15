ab397512409dda8413c7acc885bda72e7e0b58ba
jhyeok <gkswogur12@naver.com>
2023-02-15 19:59:42 +0900

    second commit

---
+++ shopping-mall/node_modules/client/src/graphql/cart.ts
@@ -0,0 +1,52 @@
+import { gql } from 'graphql-tag'
+import { Product } from './products'
+
+export type CartType = {
+    [x: string]: any
+    id: string
+    imageUrl: string
+    price: number
+    title: string
+    amount: number
+}
+
+export const ADD_CART = gql`
+  mutation ADD_CART($id: string) {
+    cart(id: $id) {
+        id 
+        imageUrl
+        price
+        title
+        amount
+    }
+  }
+`
+export const UPDATE_CART = gql`
+  mutation ADD_CART($id: string, $amount: number) {
+    cart(id: $id, amount: $amount) {
+        id
+        imageUrl
+        price
+        title
+        amount
+    }
+  }
+`
+
+export const DELETE_CART = gql`
+  mutation DELETE_CART($id: string) {
+    id
+  }
+`
+  
+export const GET_CART = gql`
+  query GET_CART {
+    id
+    imageUrl
+    price
+    title
+    amount
+  }
+`
+
+export default GET_CART
