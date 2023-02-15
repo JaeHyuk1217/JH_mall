0f4afd365d4d92f904a891dcb8286fafe2f30c27
jhyeok <gkswogur12@naver.com>
2023-02-15 18:38:45 +0900

    first upload

---
+++ shopping-mall/src/components/willPay/index.tsx
@@ -0,0 +1,38 @@
+import { SyntheticEvent } from 'react';
+import { useNavigate } from  'react-router-dom'
+import { useRecoilValue} from 'recoil'
+import { checkedCartState } from "../../recoils/cart";
+import ItemData from '../cart/itemData';
+
+const WillPay = ({
+    submitTitle,
+    handleSubmit,
+}: {
+    submitTitle: string
+    handleSubmit: (e: SyntheticEvent) => void;
+}) => {
+    const navigate = useNavigate()
+    const checkedItems = useRecoilValue(checkedCartState)
+    const totalPrice = checkedItems.reduce((res, {price, amount}) => {
+        res += price * amount
+        return res
+    }, 0)
+
+    return (
+        <div className = "cart-willpay">
+            <ul>
+                {checkedItems.map(({imageUrl, price, title, amount, id}) => (
+                    <li>
+                        <ItemData imageUrl={imageUrl} price={price} title={title} key={id} />
+                        <p>수량 : {amount}</p>
+                        <p>금액 : {price*amount}</p>
+                    </li>
+                ))}
+            </ul>
+            <p>총 금액 : {totalPrice}</p>
+            <button onClick={handleSubmit}>{submitTitle}</button>
+        </div>
+    )
+}
+
+export default WillPay
